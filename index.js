5e72f3c4061ef297484f21160d5991d0d02a83b4
RyanViers <ryanviersiv@hotmail.com>
2022-12-07 22:45:21 -0500

    firebase

---
+++ index.js
@@ -0,0 +1,20 @@
+import initializeApp from 'firebase/app';
+
+import getFirestore from 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyCRuDswIdTen_rkJbls7Kz2Z60wWbb6iDo',
+  authDomain: 'mypage-a8c32.firebaseapp.com',
+  databaseURL: 'https://mypage-a8c32-default-rtdb.firebaseio.com',
+  projectId: 'mypage-a8c32',
+  storageBucket: 'mypage-a8c32.appspot.com',
+  messagingSenderId: '948757884091',
+  appId: '1:948757884091:web:b272ea1053cc7d3ef0cd9e',
+  measurementId: 'G-JLKY6GTYNZ',
+};
+
+const app = initializeApp(firebaseConfig);
+
+const db = getFirestore(app);
+
+console.log(db);
